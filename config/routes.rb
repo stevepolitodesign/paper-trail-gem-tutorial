d70944bc100b320fbef03483ae4256cf6e2c1df8
Steve Polito <info@stevepolitodesign.com>
2020-04-13 14:27:11 -0400

    Create revert route.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     member do
       get "versions", to: "articles#versions"
       get "version/:version_id", to: "articles#version", as: "version"
+      post "revert/:version_id", to: "articles#revert", as: "revert"
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
