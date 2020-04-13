43b2f7a7cf314863597a6c24b35a250f2313a7ee
Steve Polito <info@stevepolitodesign.com>
2020-04-13 09:24:00 -0400

    Generate seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+25.times do |i|
+  Article.create(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph)
+end
