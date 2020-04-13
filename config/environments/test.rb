4ecf0e52c1f45e4a53c67fd9cce57cc9429cf8f0
Steve Polito <info@stevepolitodesign.com>
2020-04-13 10:07:23 -0400

    Configure paper_trail to work with tests.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -16,11 +16,11 @@ Rails.application.configure do
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
-    'Cache-Control' => "public, max-age=#{1.hour.to_i}"
+    "Cache-Control" => "public, max-age=#{1.hour.to_i}"
   }
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
   config.cache_store = :null_store
 
@@ -45,4 +45,9 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
+
+  # https://github.com/paper-trail-gem/paper_trail/blob/v10.3.1/README.md#7a-minitest
+  config.after_initialize do
+    PaperTrail.enabled = false
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
-require 'rails/test_help'
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
+require "rails/test_help"
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
@@ -10,4 +10,18 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  # https://github.com/paper-trail-gem/paper_trail/blob/v10.3.1/README.md#7a-minitest
+  def with_versioning
+    was_enabled = PaperTrail.enabled?
+    was_enabled_for_request = PaperTrail.request.enabled?
+    PaperTrail.enabled = true
+    PaperTrail.request.enabled = true
+    begin
+      yield
+    ensure
+      PaperTrail.enabled = was_enabled
+      PaperTrail.request.enabled = was_enabled_for_request
+    end
+  end
 end
