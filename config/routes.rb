afe0cf915c291d6eb54ad2f2798cac2acb6c534e
Steve Polito <info@stevepolitodesign.com>
2020-04-13 09:53:21 -0400

    Create a versions route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: "articles#index"
-  resources :articles
+  resources :articles do
+    member do
+      get "versions", to: "articles#versions"
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
