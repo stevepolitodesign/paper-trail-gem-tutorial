eb2ba913174ec215efb2dc5a973af67f9e4724b3
Steve Polito <info@stevepolitodesign.com>
2020-04-20 08:15:38 -0400

    Update seeds data to fix restored edge case.

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,15 @@
 end
 
 1.upto(2) do |i|
-  @deleted_article = Article.create(title: "Deleted Article #{i} Verion 1", body: Faker::Lorem.paragraph)
+  @deleted_article = Article.create(title: "Deleted Article #{i} Version 1", body: Faker::Lorem.paragraph)
   @deleted_article.destroy
   @deleted_article = Article.new(id: @deleted_article.id).versions.last.reify
   @deleted_article.save
-  @deleted_article.update(title: "Deleted Article #{i} 2")
+  @deleted_article.update(title: "Deleted Article #{i} Version 2")
   @deleted_article.destroy
 end
+
+@restored_article = Article.create(title: "A Previously Deleted Article")
+@restored_article.destroy
+@restored_article = Article.new(id: @restored_article.id).versions.last.reify
+@restored_article.save
