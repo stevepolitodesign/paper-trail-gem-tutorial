401ef376ebc3184d734724e1458e653f23ec8fe9
Steve Polito <info@stevepolitodesign.com>
2020-04-20 14:28:19 -0400

    Replace .reverse call wit call to .order(:created_at).

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -80,8 +80,8 @@ class ArticlesController < ApplicationController
 
   def deleted
     # OPTIMIZE Is this an expensive query?
-    @destroyed_versions = PaperTrail::Version.where(item_type: "Article", event: "destroy")
-    @latest_destroyed_versions = @destroyed_versions.filter { |v| v.reify.versions.last.event == "destroy" }.map(&:reify).reverse.uniq(&:id)
+    @destroyed_versions = PaperTrail::Version.where(item_type: "Article", event: "destroy").order(created_at: :desc)
+    @latest_destroyed_versions = @destroyed_versions.filter { |v| v.reify.versions.last.event == "destroy" }.map(&:reify).uniq(&:id)
     @articles = @latest_destroyed_versions
   end
 
